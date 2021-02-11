3292e10ea69f1b82f74a19adbe40a2290f37bf6e
Sree <dsrilu@gmail.com>
2021-02-11 11:09:47 +0530

    added extent reports

--- src/test/java/com/ui/testrunners/TestRunner.java
+++ src/test/java/com/ui/testrunners/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 		features={"src/test/resources/com/ui/features/"},
 		monochrome=true,
 		glue= {"com.ui.stepdefinitions","com.ui.hooks"},
-		plugin= {"pretty"}
+		plugin=  {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 		)
 public class TestRunner  extends AbstractTestNGCucumberTests{
 	@Test
