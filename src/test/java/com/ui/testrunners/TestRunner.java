bd51a8908ac3c0c72503aa35970ac1c5f053cd37
Sree <dsrilu@gmail.com>
2021-02-11 13:02:52 +0530

    checking jenkins build

--- src/test/java/com/ui/testrunners/TestRunner.java
+++ src/test/java/com/ui/testrunners/TestRunner.java
@@ -15,10 +15,10 @@ import io.cucumber.testng.CucumberOptions;
 				"rerun:target/failedrerun.txt"}
 		)
 public class TestRunner  extends AbstractTestNGCucumberTests{
-	//@Override
-//	@DataProvider(parallel=true)
-//	public Object[][] scenarios(){
-//		return super.scenarios();
-//	}
+	@Override
+	@DataProvider(parallel=true)
+	public Object[][] scenarios(){
+		return super.scenarios();
+	}
 }
 
